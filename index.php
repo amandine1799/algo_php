6480280defacb656d586b7f30b5e5ac1fd7c9ff9
root <a.gaignere@simplon-charleville.fr>
2019-04-24 09:25:44 +0200

    step 8

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Ast PHP</title>
+    </head>
+    <body>
+      <?php
+      $x = 1;
+      $a = " petite perruche";
+      while($x <= 30){
+        echo ($x);
+        echo ($a. '<br/>');
+        $x++;
+      }
+      ?>
+    </body>
+</html>
